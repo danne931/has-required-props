81100e0e073cddb594c5e21b49d309037e6031d9
Daniel Eisenbarger <danne9.31@gmail.com>
2016-07-21 12:34:29 -0700

    .

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const _hasRequiredProps = requiredProps => props =>
     : requiredProps.every(key => get(props, key) !== undefined)
 
 export default function hasRequiredProps (requiredProps, props) {
-  if (arguments.length === 0 || isEmptyObject(props)) return false
+  if (isEmptyObject(props)) return false
   if (isEmptyObject(requiredProps)) return true
-  if (typeof props === 'string' && props.trim() === '') return false
+  if (typeof requiredProps === 'string' && requiredProps.trim() === '') return false
 
   const check = _hasRequiredProps(requiredProps)
   return Array.isArray(props) ? props.every(check) : check(props)
